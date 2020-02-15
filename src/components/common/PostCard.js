315c0c5540713f6dacd6b342b9545fd48a34d386
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-15 13:23:32 -0500

    [Update] postcard

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -6,14 +6,13 @@ import { readingTime as readingTimeHelper } from '@tryghost/helpers'
 
 const PostCard = ({ post }) => {
     const url = `/${post.slug}/`
-    const readingTime = readingTimeHelper(post)
 
     return (
         <Link to={url} className="post-card">
             <header className="post-card-header">
                 {post.feature_image &&
                     <div className="post-card-image" style={{
-                        backgroundImage: `url(${post.feature_image})` ,
+                        backgroundImage: `url(${post.feature_image})`,
                     }}></div>}
                 {post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
                 {post.featured && <span>Featured</span>}
@@ -24,14 +23,14 @@ const PostCard = ({ post }) => {
                 <div className="post-card-footer-left">
                     <div className="post-card-avatar">
                         {post.primary_author.profile_image ?
-                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
+                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name} /> :
+                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name} />
                         }
                     </div>
-                    <span>{ post.primary_author.name }</span>
+                    <span>{post.primary_author.name}</span>
                 </div>
                 <div className="post-card-footer-right">
-                    <div>{readingTime}</div>
+                    <div>Leer mas</div>
                 </div>
             </footer>
         </Link>
